b2f2e28cb36d424fb0adc8bf26a1c5b8e579c07f
A-Random-Human <70415062+A-Random-Human@users.noreply.github.com>
2021-11-08 15:45:05 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 #-----import statements-----
 import turtle as trtl
 import random as rand
+import leaderboard as lb
+
+#Input from User
+player_name = input ("Please enter your name: ")
 
 #-----game configuration----
 font_setup = ("Arial", 20, "bold")
@@ -18,11 +22,13 @@ spot.fillcolor(spotColor)
 spot.pencolor("pink")
 spot.penup()
 
+#Score variables
 score_writer = trtl.Turtle()
 score_writer.hideturtle()
 score_writer.penup()
 score_writer.goto(60,120)
 
+#timer variables
 timer = 10
 counter_interval = 500   
 timer_up = False
@@ -31,9 +37,15 @@ counter.hideturtle()
 counter.penup()
 counter.goto(-200,120)
 
+# leaderboard variables
+leaderboard_file_name = "a122_leaderboard.txt"
+leader_names_list = []
+leader_scores_list = []
+
 wn = trtl.Screen()
 trtl.screensize(400, 300, "YellowGreen")
 #-----game functions--------
+
 def change_position():
   new_xpos = rand.randint(-200,200)
   new_ypos = rand.randint(-150,135)
@@ -48,13 +60,18 @@ def spot_clicked(x,y):
     change_size()
     change_position()
   else:
+    spot.goto(-20,0)
+    spot.turtlesize(10)
     spot.shape("turtle")
     spot.fillcolor("FireBrick")
     trtl.bgcolor ("Yellow")
     
 def change_size():
   global spotSize
-  spotSize -=1
+  if spotSize > 0:
+    spotSize -=1
+  else:
+    spotSize  +=1
   spot.turtlesize(spotSize)
 
 def update_score():
@@ -63,12 +80,28 @@ def update_score():
   score_writer.clear()
   score_writer.write("Score: " + str(score), font=font_setup)
 
+def manage_leaderboard():
+  global leader_scores_list
+  global leader_names_list
+  global score
+  global spot
+
+  lb.load_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list)
+  
+  if (len(leader_scores_list) < 5 or score > leader_scores_list[4]):
+    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+    lb.draw_leaderboard(leader_names_list, leader_scores_list, True, spot, score)
+    
+  else:
+    lb.draw_leaderboard(leader_names_list, leader_scores_list, False, spot, score)
+
 def countdown():
   global timer, timer_up
   counter.clear()
   if timer <= 0:
     counter.write("Time's Up", font=font_setup)
     timer_up = True
+    #manage_leaderboard()
   else:
     counter.write("Timer: " + str(timer), font=font_setup)
     timer -= 1
